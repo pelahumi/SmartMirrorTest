62276cea494e3dbbe0030fe13ed301215277c7c8
Pelayo <phuermij@myuax.com>
2023-12-11 10:58:53 +0100

    prueba

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-LOGIN_REDIRECT_URL = 'confirmacion'
+
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
