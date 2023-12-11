1c5c75278e99e888b5dd3dd224d4530c04f8ad3e
Pelayo <phuermij@myuax.com>
2023-12-11 09:03:12 +0100

    cambios

--- webpersonal/core/urls.py
+++ webpersonal/core/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("login/", views.login, name="login"),
     path("registro/", views.registro, name="registro"),
-    path("confirmacion/api/", views.api, name="api"),
-    path('confirmacion/api/noticias/', views.noticias, name='noticias'),
-    path('confirmacion/api/pregunta-ciudad/', pregunta_ciudad, name='pregunta_ciudad'),
-    path('confirmacion/api/clima/', obtener_clima, name='obtener_clima'),
+    path("api/", views.api, name="api"),
+    path('api/noticias/', views.noticias, name='noticias'),
+    path('api/pregunta-ciudad/', pregunta_ciudad, name='pregunta_ciudad'),
+    path('api/clima/', obtener_clima, name='obtener_clima'),
 ]
 
